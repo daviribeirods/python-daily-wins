9cd770e52dd2578f705e0cdb1e3fe0e27ccafc69
Davi Santos <22971355+daviribeirods@users.noreply.github.com>
2026-07-23 16:45:04 -0300

    ADD: remove minimum

---
+++ python_solutions/remove_minimum.py
@@ -0,0 +1,17 @@
+# https://www.codewars.com/kata/563cf89eb4747c5fb100001b
+
+def remove_smallest(numbers: list) -> list:
+    if not numbers:
+        return []
+    
+    smallest_rating = 100_000_000
+    for i in numbers:
+        if i <= smallest_rating:
+            smallest_rating = i
+
+    new_array = numbers.copy()
+    new_array.remove(smallest_rating)
+
+    return new_array
+
+print(remove_smallest([5, 5, 1, 1, 2, 3]))
